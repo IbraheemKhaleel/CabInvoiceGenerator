7d32e117e2f2610705ca919f85aa4f9ce82155b8
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-06 08:11:29 +0530

    Added : test code for checking invoice of multiple rides using arrays

--- src/test/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGeneratorTest.java
@@ -11,6 +11,17 @@ public class CabInvoiceGeneratorTest {
         double distance = 10.05 ;
         int time = 6 ;
         double fare = cabInvoiceGenerator.calculateTotalFare(distance, time) ;
-        Assert.assertEquals(106 ,fare,0.5);
+        Assert.assertEquals(106 , fare,0.5);
+    }
+
+    @Test
+    public void givenMultipleRides_When_Calculated_ShouldReturnTotalFare()
+    {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        Rides[] ride = {new Rides(2.0 , 3),
+                        new Rides(3.0, 4),
+                        new Rides(4.0, 5)} ;
+        double fare = cabInvoiceGenerator.calculateMultipleRides(ride) ;
+        Assert.assertEquals(2*10 + 3 + 3*10 + 4 + 4*10 + 5, fare, 0.0 ) ;
     }
 }
